6b1c8b43e38afb193857d53fe70950bcac79667f
anajuliaceag <142040048+anajuliaceag@users.noreply.github.com>
2025-06-10 10:51:15 -0300

    Add files via upload

---
+++ exercicio5/script5.js
@@ -0,0 +1,8 @@
+let num1 = Number(window.prompt("Digite aqui o primeiro número:"));
+let num2 = Number(window.prompt("Digite aqui o segundo número: "));
+
+if(num1 > num2){
+    alert(num1 = " É maior que" + num2);
+}else{
+    alert(num2 = " É maior que" + num2);
+}
